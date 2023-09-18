68b418815fed4b35a4c6a8aa5ad99fb99d0f1468
Jonathan Wolf <jonathan.wolf@showplacehq.com>
2023-09-17 20:34:46 -0500

    percentage change and redirect

--- app/components/PieChart.jsx
+++ app/components/PieChart.jsx
@@ -66,7 +66,7 @@ const renderCustomizedLegend = ({ payload }) => {
                 {formatDollar(entry.payload.value)}
               </td>
               <td className="px-1 py-2 whitespace-nowrap">
-                {entry.payload.percentage}%
+                {Math.round(entry.payload.percentage, 0)}%
               </td>
             </tr>
           ))}
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/login',
+        destination: '/transactions?selected_identifier=all',
         permanent: true,
       },
     ];
